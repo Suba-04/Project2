2ade60e567e5f2502227ac22a06e0e638b460ae2
Suba-04 <132046005+Suba-04@users.noreply.github.com>
2024-04-04 23:55:57 +0530

    1

---
+++ demo.js
@@ -0,0 +1,62 @@
+{
+    
+    let x=10;
+    console.log(x)
+    }
+    var y=20;
+    const z=30;
+    
+    console.log(y/z)
+    
+    var price=100;
+    var product="briyani";
+    var tax=8.7;
+    console.log(product,tax)
+    
+    var M="si";
+    console.log(typeof(M))
+    
+    var a=2;
+    var b=5;
+    b++
+    console.log(b);
+    
+    var cars=["BMW","HUI"];
+    console.log(cars[1])
+
+    function hi(){
+console.log("hello")
+    }
+    hi()
+
+    var c=2
+    var d=4
+    function add(){
+        console.log(a+b)
+    }
+    add()
+    
+        var actor="Kamal"
+        var Player="Dhoni"
+        var Film="Anbe sivam"
+        console.log("Actor:"+actor)
+
+       function area(l,b){
+        console.log(l*b)
+
+       }
+       area(3,4)
+       if(score=50)
+        {
+            console.log("Need to prove")
+        }
+        else if(score>50 && score<70)
+        {
+            console.log("Good job")
+        }
+        else{
+            console.log("c")
+        }
+   
+
+    
